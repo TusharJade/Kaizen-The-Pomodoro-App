49ed60c5cefca6039777eece067bacc1ac40175f
TusharJade <79801546+TusharJade@users.noreply.github.com>
2022-07-20 15:59:30 +0530

    feat: started working on pomdoro clock

--- src/pages/PomodoroTimerPage/PomodoroTimerPage.jsx
+++ src/pages/PomodoroTimerPage/PomodoroTimerPage.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
 import "./PomodoroTimerPage.css";
 
 const PomodoroTimerPage = () => {
-  return <></>;
+  const [modes, setModes] = useState("Work");
+  return (
+    <section className="pomodoro-clock-section">
+      <div className="pomodoro-text">Pomodoro clock</div>
+      <div className="pomodoro-btns-container">
+        <button
+          className={`pomodoro-btns ${
+            modes === "Work" ? "pomo-btn-active" : null
+          }`}
+          onClick={() => setModes("Work")}
+        >
+          Work
+        </button>
+        <button
+          className={`pomodoro-btns ${
+            modes === "Short break" ? "pomo-btn-active" : null
+          }`}
+          onClick={() => setModes("Short break")}
+        >
+          Short break
+        </button>
+        <button
+          className={`pomodoro-btns ${
+            modes === "Long break" ? "pomo-btn-active" : null
+          }`}
+          onClick={() => setModes("Long break")}
+        >
+          Long break
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export { PomodoroTimerPage };
